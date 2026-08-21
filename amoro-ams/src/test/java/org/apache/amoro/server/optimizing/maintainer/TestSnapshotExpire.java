8002cfe8392ef9fee086e4b8ac20a331836455d5
badalprasadsingh <badal@datazip.io>
2026-08-21 14:40:32 +0530

    minor
    Signed-off-by: badalprasadsingh <badal@datazip.io>

--- amoro-ams/src/test/java/org/apache/amoro/server/optimizing/maintainer/TestSnapshotExpire.java
+++ amoro-ams/src/test/java/org/apache/amoro/server/optimizing/maintainer/TestSnapshotExpire.java
@@ -14,8 +14,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Modified by Datazip Inc. in 2026
  */
 
 package org.apache.amoro.server.optimizing.maintainer;
@@ -264,7 +262,7 @@ public class TestSnapshotExpire extends ExecutorTestBase {
 
     TestTableMaintainerContext.Impl context =
         new TestTableMaintainerContext.Impl(
-            TestTableMaintainerContext.parseTestTableConfig(table.properties()), table);
+            TableConfigurations.parseTableConfig(table.properties()), table);
     new MixedTableMaintainer(table, context).expireSnapshots();
     Assert.assertEquals(1, Iterables.size(table.snapshots()));
 
